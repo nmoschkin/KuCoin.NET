0bd31ac17ddf50cac1338d1cc0bab09d5a3f3c1d
Nathaniel Moschkin <ironywrit@gmail.com>
2021-07-05 18:29:40 -0400

    Improve console example app.

--- KuCoinConsole/Program.cs
+++ KuCoinConsole/Program.cs
@@ -12,6 +12,7 @@ namespace KuCoinConsole
     {
         static Dictionary<string, Level3Observation> observers = new Dictionary<string, Level3Observation>();
 
+        static Level3 l3conn;
         static void Main(string[] args)
         {
 
@@ -28,48 +29,107 @@ namespace KuCoinConsole
             }
 
 
-            var l3 = new Level3(cred);
+            l3conn = new Level3(cred);
 
-            Console.Write("Connecting...");
+            // We want to monitor how fast.
+            l3conn.MonitorThroughput = true;
+            
+            // Disable Observable book updating for console app.
+            l3conn.UpdateInterval = 0;
 
-            l3.Connect().ContinueWith(async (t) =>
+            // clear the pin off the screen
+            Console.Clear();
+
+            Console.WriteLine("Connecting...");
+
+            string[] syms = new string[] { "ETH-USDT", "XLM-USDT", "BTC-USDT", "ADA-USDT", "DOGE-USDT" };
+
+            l3conn.Connect().ContinueWith(async (t) =>
             {
-                Console.WriteLine("Subscribing to ETH-USDT...");
-                Console.WriteLine("");
-                Console.WriteLine("");
+                foreach (var sym in syms)
+                {
+                    Console.WriteLine($"Subscribing to {sym} ...");
 
-                observers.Add("ADA-USDT", await l3.AddSymbol("ADA-USDT"));
-                observers.Add("BTC-USDT", await l3.AddSymbol("BTC-USDT"));
+                    try
+                    {
+                        var obs = await l3conn.AddSymbol(sym);
+                        observers.Add(sym, obs);
+                    }
+                    catch { }
+                }
 
                 foreach (var obs in observers)
                 {
                     obs.Value.NextObject += Observer_NextObject;
                 }
 
+                // clear console to display data.
+                Console.Clear();
+                Console.CursorVisible = false;
+
             }).Wait();
 
-            while (l3?.Connected ?? false)
+            while (l3conn?.Connected ?? false)
             {
+                // loop forever to keep the program alive.
                 Task.Delay(5).Wait();
             }
 
         }
 
-        static decimal oba = 0M, obb = 0M, ba = 0M, bb = 0M;
+        // keep track of time
+        static long? updTime = null;
 
         private static void Observer_NextObject(Kucoin.NET.Data.Websockets.Level3Update data)
         {
-            ba = observers[data.Symbol].FullDepthOrderBook.Asks[0].Price;
-            bb = observers[data.Symbol].FullDepthOrderBook.Bids[0].Price;
 
-            if (ba != oba || bb != obb)
+            // We don't want to update the screen every single time we get new data.
+            // If we do that, the data buffer will grow larger and we will not get to the end.
+            // We need to give the thread time to catch up with the buffering thread.
+            // So we will do every 100 milliseconds update.
+            if (updTime != null)
             {
-                Console.Write($"Best Ask: {ba:#,###.00} : Best Bid: {bb:#,###.00} {data.Symbol}                        \r");
+                if (DateTime.UtcNow.Ticks - updTime < (100 * 10_000)) return;
             }
 
-            oba = ba;
-            obb = bb;
+            updTime = DateTime.UtcNow.Ticks;
+            // 
 
+            decimal ba, bb;
+            
+            // remember cursor position
+            var x = Console.CursorTop;
+
+            Console.WriteLine($"{data.Timestamp:G}                 ");
+            Console.WriteLine($"                                   ");
+
+            foreach (var obs in observers)
+            {
+                if (obs.Value.FullDepthOrderBook is null) continue;
+
+                ba = obs.Value.FullDepthOrderBook.Asks[0].Price;
+                bb = obs.Value.FullDepthOrderBook.Bids[0].Price;
+
+                var text = $"{MinChars(obs.Value.Symbol, 9)} - Best Ask: {MinChars(ba.ToString("#,##0.00######"), 16)} : Best Bid: {MinChars(bb.ToString("#,##0.00######"), 16)}                         ";
+
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("                                                           ");
+            Console.WriteLine($"Throughput: {PrintFriendlySpeed((ulong)l3conn.Throughput)}                           ");
+
+            // restore cursor position
+            Console.CursorTop = x;
+        }
+
+        public static string MinChars(string text, int minChars)
+        {
+            var o = text;
+
+            if (text.Length >= minChars) return o;
+            o += new string(' ', minChars - text.Length);
+
+            return o;
         }
 
         public static string Hvcyp
@@ -88,6 +148,123 @@ namespace KuCoinConsole
         public static Guid Seed
         {
             get => Guid.Parse(Hvcyp);
+        }
+
+        /// <summary>
+        /// Prints a number value as a friendly byte speed in TiB, GiB, MiB, KiB or B.
+        /// </summary>
+        /// <param name="speed">The speed to format.</param>
+        /// <param name="format">Optional numeric format for the resulting value.</param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public static string PrintFriendlySpeed(ulong speed, string format = null, bool binary = false)
+        {
+            double fs;
+            double spd = speed;
+            string nom;
+
+            if (binary)
+            {
+                if ((spd >= (1024L * 1024 * 1024 * 1024 * 1024 * 1024)))
+                {
+                    fs = (spd / (1024L * 1024 * 1024 * 1024 * 1024 * 1024));
+                    nom = "Eb/s";
+                    //wow
+                }
+                else if ((spd >= (1024L * 1024 * 1024 * 1024 * 1024)))
+                {
+                    fs = (spd / (1024L * 1024 * 1024 * 1024 * 1024));
+                    nom = "Pb/s";
+                    //wow
+                }
+                else if ((spd >= (1024L * 1024 * 1024 * 1024)))
+                {
+                    fs = (spd / (1024L * 1024 * 1024 * 1024));
+                    nom = "Tb/s";
+                    //wow
+                }
+                else if ((spd >= (1024 * 1024 * 1024)))
+                {
+                    fs = (spd / (1024 * 1024 * 1024));
+                    nom = "Gb/s";
+                    // still wow
+                }
+                else if ((spd >= (1024 * 1024)))
+                {
+                    fs = (spd / (1024 * 1024));
+                    nom = "Mb/s";
+                    // okay
+                }
+                else if ((spd >= (1024)))
+                {
+                    fs = (spd / (1024));
+                    nom = "Kb/s";
+                    // fine.
+                }
+                else
+                {
+                    fs = spd;
+                    nom = "b/s";
+                    // wow.
+                }
+
+            }
+            else
+            {
+                if ((spd >= (1000L * 1000 * 1000 * 1000 * 1000 * 1000)))
+                {
+                    fs = (spd / (1000L * 1000 * 1000 * 1000 * 1000 * 1000));
+                    nom = "Eb/s";
+                    //wow
+                }
+                else if ((spd >= (1000L * 1000 * 1000 * 1000 * 1000)))
+                {
+                    fs = (spd / (1000L * 1000 * 1000 * 1000 * 1000));
+                    nom = "Pb/s";
+                    //wow
+                }
+                else if ((spd >= (1000L * 1000 * 1000 * 1000)))
+                {
+                    fs = (spd / (1000L * 1000 * 1000 * 1000));
+                    nom = "Tb/s";
+                    //wow
+                }
+                else if ((spd >= (1000 * 1000 * 1000)))
+                {
+                    fs = (spd / (1000 * 1000 * 1000));
+                    nom = "Gb/s";
+                    // still wow
+                }
+                else if ((spd >= (1000 * 1000)))
+                {
+                    fs = (spd / (1000 * 1000));
+                    nom = "Mb/s";
+                    // okay
+                }
+                else if ((spd >= (1000)))
+                {
+                    fs = (spd / (1000));
+                    nom = "Kb/s";
+                    // fine.
+                }
+                else
+                {
+                    fs = spd;
+                    nom = "b/s";
+                    // wow.
+                }
+
+            }
+
+            if (format != null)
+            {
+                return System.Math.Round(fs, 2).ToString(format) + " " + nom;
+            }
+            else
+            {
+                return System.Math.Round(fs, 2) + " " + nom;
+            }
+
         }
     }
 }
