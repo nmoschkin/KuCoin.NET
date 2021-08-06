c24815b7a18b496e57c360c464d8b90ef763027e
Nathaniel Moschkin <ironywrit@gmail.com>
2021-08-06 00:15:10 -0400

    Added INotifyPropertyChanging support

--- Kucoin.NET/Observable/ObservableBase.cs
+++ Kucoin.NET/Observable/ObservableBase.cs
@@ -1,6 +1,4 @@
 ﻿
-using Kucoin.NET.Helpers;
-
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,13 +11,67 @@ using System.Threading.Tasks;
 
 namespace Kucoin.NET.Observable
 {
+
+    /// <summary>
+    /// Provide data for the <see cref="ObservableBase.PropertyChanging"/> event.
+    /// </summary>
+    public class ObservablePropertyChangingEventArgs : PropertyChangingEventArgs
+    {
+        /// <summary>
+        /// Gets or sets a value that indicates that the property change should be cancelled.
+        /// </summary>
+        public bool Cancel { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating that the event has been handled, and the remaining handlers should not be invoked.
+        /// </summary>
+        public bool Handled { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObservablePropertyChangingEventArgs"/> class.
+        /// </summary>
+        /// <param name="propertyName">The name of the property.</param>
+        public ObservablePropertyChangingEventArgs(string propertyName) : base(propertyName)
+        {
+        }
+    }
+
     /// <summary>
     /// Base class for observable classes.
     /// </summary>
-    public abstract class ObservableBase : INotifyPropertyChanged
-    {
+    public abstract class ObservableBase : INotifyPropertyChanged, INotifyPropertyChanging
+    { 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        private List<EventHandler<ObservablePropertyChangingEventArgs>> invokerList1 = new List<EventHandler<ObservablePropertyChangingEventArgs>>();
+        private List<PropertyChangingEventHandler> invokerList2 = new List<PropertyChangingEventHandler>();
+
+        /// <summary>
+        /// Occurs when a property value is changing.
+        /// </summary>
+        public event EventHandler<ObservablePropertyChangingEventArgs> PropertyChanging
+        {
+            add
+            {
+                invokerList1.Add(value);
+            }
+            remove
+            {
+                invokerList1.Remove(value);
+            }
+        }
+
+        event PropertyChangingEventHandler INotifyPropertyChanging.PropertyChanging
+        {
+            add
+            {
+                invokerList2.Add(value);
+            }
+            remove
+            {
+                invokerList2.Remove(value);
+            }
+        }
 
         /// <summary>
         /// Set a property value if the current value is not equal to the new value and raise the <see cref="INotifyPropertyChanged.PropertyChanged"/> event.
@@ -29,6 +81,7 @@ namespace Kucoin.NET.Observable
         /// <param name="value">The new value.</param>
         /// <param name="propertyName">The property name.</param>
         /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected virtual bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = null)
         {
             bool pass;
@@ -58,7 +111,10 @@ namespace Kucoin.NET.Observable
 
             if (pass)
             {
+                if (OnPropertyChanging(propertyName)) return false;
+
                 backingStore = value;
+
                 OnPropertyChanged(propertyName);
             }
 
@@ -69,12 +125,39 @@ namespace Kucoin.NET.Observable
         /// Raise <see cref="INotifyPropertyChanged.PropertyChanged"/>.
         /// </summary>
         /// <param name="propertyName"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-    }
+        /// <summary>
+        /// Raise <see cref="INotifyPropertyChanged.PropertyChanged"/>.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <returns>True if the event was cancelled.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected bool OnPropertyChanging([CallerMemberName] string propertyName = null)
+        {
+            if (invokerList1.Count == 0 && invokerList2.Count == 0) return false;
 
+            var e = new ObservablePropertyChangingEventArgs(propertyName);
+
+            foreach (var invoker1 in invokerList1)
+            {
+                invoker1(this, e);
+                if (e.Handled) return e.Cancel;
+            }
+
+            foreach (var invoker2 in invokerList2)
+            {
+                invoker2(this, e);
+                if (e.Handled) break;
+            }
+
+            return e.Cancel;
+        }
+
+    }
 
 }
