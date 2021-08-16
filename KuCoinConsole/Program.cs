717ee465a5bfa5a5ba7b56b9fb80df4d2ccacd73
Nathaniel Moschkin <ironywrit@gmail.com>
2021-08-15 21:42:27 -0400

    Cleanup

--- KuCoinConsole/Program.cs
+++ KuCoinConsole/Program.cs
@@ -543,7 +543,7 @@ namespace KuCoinConsole
                     text += $"\r\n{MinChars("", maxSymbolLen)} - Match Share: {MinChars(mpcts[z].ToString("##0") + "%", 4)}   Total Share: {MinChars(pcts[z++].ToString("##0") + "%", 4)}   State: " + MinChars(l3.State.ToString(), 14) + "  Queue Length: " + MinChars(l3.QueueLength.ToString(), 10);
                     text += "\r\n                                                                                                         ";
 
-                    itemTexts.Add(text + "\r\n");
+                    itemTexts.Add(text + "\r\n                                                                                                                                                   ");
                     readOut.AppendLine(text);
                 }
 
