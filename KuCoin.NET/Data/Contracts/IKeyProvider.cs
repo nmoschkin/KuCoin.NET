d52f4e246888ad5220938feac99a977e04c136d3
Nathaniel Moschkin <theimprobable@hotmail.com>
2022-04-09 11:42:59 -0400

    IKeyProvider

---
+++ KuCoin.NET/Data/Contracts/IKeyProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuCoin.NET.Data.Market
+{
+    public interface IKeyProvider<T>
+    {
+
+        T Key { get; }
+    }
+}
